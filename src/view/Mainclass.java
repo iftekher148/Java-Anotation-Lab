22e5eb6654ca1b2674be5793aee9f8e63755fb44
iftekher <rocktimcseru50@gmail.com>
2018-10-29 13:19:03 +0600

    Anotation in calculator java project

---
+++ src/view/Mainclass.java
@@ -0,0 +1,17 @@
+package view;
+
+/**
+ * Open a package for main
+ * Open a class for main
+ * @author ess
+ *
+ */
+public class Mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 new Calculators();
+
+	}
+
+}
